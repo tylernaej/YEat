2913731345c107436e208e9743fd477b9ae9db0a
Connor Lam <connorlam03@yahoo.com>
2022-09-17 13:34:10 -0700

    done with users

--- react-app/src/components/User.js
+++ react-app/src/components/User.js
@@ -4,7 +4,7 @@ import { useParams } from 'react-router-dom';
 function User() {
   const [user, setUser] = useState({});
   const { userId }  = useParams();
-
+  console.log(user)
   useEffect(() => {
     if (!userId) {
       return;
