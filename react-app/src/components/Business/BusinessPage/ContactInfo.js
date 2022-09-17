0ae82ad1d2974d08f82765799d7a011e3944205f
tyler <tylernaej@gmail.com>
2022-09-17 19:03:36 -0400

    added contract info component

--- react-app/src/components/Business/BusinessPage/ContactInfo.js
+++ react-app/src/components/Business/BusinessPage/ContactInfo.js
@@ -3,15 +3,15 @@ import React from "react";
 
 function ContactInfo({ business }){
     
-    console.log(business)
-
     return (
         <div>
             <div>
                 {business.website}
+                <i class="fa-solid fa-arrow-up-right-from-square"></i>
             </div>           
             <div>
                 {business.phone}
+                <i class="fa-solid fa-phone-flip"></i>
             </div>
             <div>
                 potential map info?
