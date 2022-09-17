caf7452b580befbfa86e38c42ede3c7ddf08398a
tyler <tylernaej@gmail.com>
2022-09-17 11:10:39 -0400

    added get all amenities route

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .api.business_routes import business_routes
 from .api.review_routes import review_routes
+from .api.amenity_routes import amenity_routes
 
 from .seeds import seed_commands
 
@@ -35,6 +36,7 @@ app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(business_routes, url_prefix='/api/businesses')
 app.register_blueprint(review_routes, url_prefix='/api/reviews')
+app.register_blueprint(amenity_routes, url_prefix='/api/amenities')
 db.init_app(app)
 Migrate(app, db)
 
