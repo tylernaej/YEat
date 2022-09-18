39c1bc79c094845d3cc8ea1115a14a8904894dc5
Connor Lam <connorlam03@yahoo.com>
2022-09-18 13:16:39 -0700

    update review thunk done

--- react-app/src/store/reviews.js
+++ react-app/src/store/reviews.js
@@ -4,7 +4,7 @@ const GET_BIZ_REVIEWS = "biz/get-biz-reviews"
 // const GET_USERS_REVIEWS = "biz/get-users-reviews"
 
 const CREATE_REVIEW = "biz/create-review"
-const READ_REVIEW = "biz/read-review"
+// const READ_REVIEW = "biz/read-review"
 const UPDATE_REVIEW = "biz/update-review"
 const DELETE_REVIEW = "biz/delete-review"
 
@@ -31,12 +31,12 @@ const createReviewAction = payload => {
     }
 }
 
-const readReviewAction = payload => {
-    return {
-        type: READ_REVIEW,
-        payload
-    }
-}
+// const readReviewAction = payload => {
+//     return {
+//         type: READ_REVIEW,
+//         payload
+//     }
+// }
 
 const updateReviewAction = payload => {
     return {
@@ -85,6 +85,24 @@ export const createReviewThunk = ({businessId, review}) => async dispatch => {
     return data
 }
 
+export const updateReviewThunk = ({reviewId, review}) => async dispatch => {
+    const response = await fetch(
+        `/api/review/${reviewId}`,
+        {
+            method: "PUT",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(review)
+        }
+    )
+    const data = await response.json()
+
+    if (response.ok) {
+        await dispatch(updateReviewAction)
+    }
+
+    return data
+}
+
 // Reducer
 
 const intitialState = {}
@@ -99,7 +117,13 @@ const reviewsReducer = (state = intitialState, action) => {
             return newState
         }
         case (CREATE_REVIEW): {
+            newState = {...state}
             newState[action.payload.id] = action.payload
+            return newState
+        }
+        case (UPDATE_REVIEW): {
+            newState = {...state}
+            newState[action.payload.id] = {...newState[action.payload.id], ...action.payload}
             return newState
         }
         default: {
