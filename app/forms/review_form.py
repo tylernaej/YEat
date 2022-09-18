c2c6e5fe936fdb763ee6d58f36872a5035f9508c
Edward Felipe III <edomfel3@gmail.com>
2022-09-18 11:49:55 -1000

    PUT request for editing a review complete (backend)

---
+++ app/forms/review_form.py
@@ -0,0 +1,16 @@
+from ast import Num
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField
+from wtforms.validators import DataRequired, Email, ValidationError, NumberRange
+
+from app.models import review
+
+#validators
+def review_length(form, field):
+    review = field.data
+    if len(review) < 15:
+        raise ValidationError('Description must be 10 characters or more')
+
+class ReviewForm(FlaskForm):
+    review = StringField('Review', validators=[DataRequired(), review_length])
+    rating = IntegerField('Rating', validators=[DataRequired(), NumberRange(min=1, max=5, message="Rating must be between 1-5")])
