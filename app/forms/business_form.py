a7843c5a306f04143298dc1c848a8563ab019c81
Connor Lam <connorlam03@yahoo.com>
2022-09-20 16:58:39 -0700

    added custom validators to backend

--- app/forms/business_form.py
+++ app/forms/business_form.py
@@ -1,15 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField
-from wtforms.validators import DataRequired, Email, ValidationError
+from wtforms.validators import DataRequired, Email, ValidationError, NumberRange
 from app.models import Business
 
 # validations
 def description_length(form, field):
     description = field.data
-    if len(description) > 2000:
-        raise ValidationError('Description must be 2000 characters or less')
-    elif len(description) < 10:
-        raise ValidationError('Description must be 10 characters or more')
+    if len(description) < 10 or len(description) > 2000:
+        raise ValidationError('Description must be between 10 and 2000 characters')
 
 
 def valid_lat(form, field):
@@ -22,6 +20,12 @@ def valid_lng(form, field):
     longitude = field.data
     if longitude > 180 or longitude < -180:
         raise ValidationError('Invalid longitude, please be between 180 and -180')
+
+
+def name_length(form, field):
+    name = field.data
+    if len(name) > 50:
+        raise ValidationError('Name must be less than 50 characters')
 
 
 # def valid_email(form, field):
@@ -40,7 +44,7 @@ class CreateBusinessForm(FlaskForm):
     latitude = IntegerField('Latitude', validators=[DataRequired(), valid_lat])
     longitude = IntegerField("Longitude", validators=[DataRequired(), valid_lng])
     description = StringField('Description', validators=[DataRequired(), description_length])
-    priceRange = IntegerField('Price Range', validators=[DataRequired()])
+    priceRange = IntegerField('Price Range', validators=[DataRequired(), NumberRange(min=1, max=4, message="Rating must be between 1-4")])
     email = StringField('Email', validators=[DataRequired(), Email()])
     phone = StringField('Phone Number', validators=[DataRequired()])
     name = StringField('Name', validators=[DataRequired()])
