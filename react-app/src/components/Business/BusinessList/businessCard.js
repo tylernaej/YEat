71cd2bd597d4a0e7b3caa825fedd8d4c8a1a0cc7
Connor Lam <connorlam03@yahoo.com>
2022-09-20 22:12:57 -0700

    biz list almost done

--- react-app/src/components/Business/BusinessList/businessCard.js
+++ react-app/src/components/Business/BusinessList/businessCard.js
@@ -2,25 +2,39 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import "./businessCard.css"
 
+function dollarsigns(range) {
+  let signs = "";
+  for (let i = 0; i < Number(range); i++) {
+    signs = signs.concat("$");
+  }
+  return signs;
+}
+
 
 function BizCard({ business }) {
-    console.log(`Loading a biz card for: ${business.name}`)
+    console.log(business)
+    console.log(`${(business.avgReviews / 5) * 100}`)
+    const price = dollarsigns(business.priceRange);
     return (
         <div className='each-card'>
             <NavLink className="flex-row navLink" to={`/businesses/${business.id}/about`}>
-                <div style={{height: "205px", minWidth:"205px", border:"1px red solid"}}>
+                <div style={{height: "205px", minWidth:"205px"}}>
                     <img src='https://wallpapercave.com/wp/wp2416473.jpg' style={{objectFit: "cover", height:"205px", width:"205px"}} alt=''/>
                     {/* photo here */}
                 </div>
-                <div>
-                    <p> {business.name} </p>
-                    <div class="stars-outer">
-                        <div class="stars-inner" style={{width: `${(business.avgReviews / 5) * 100}%`}}></div>
+                <div className='biz-card-right-side'>
+                    <div className='biz-card-name'> {business.name} </div>
+                    <div className='review-section'>
+                        <div class="stars-outer">
+                            <div class="stars-inner" style={{width: `${(business.avgReviews / 5) * 100}%`}}></div>
+                        </div>
+                        <div>{business.numReviews}</div>
                     </div>
-                    <div className='flex-row'>
+                    <div className='flex-row-align-center'>
                         {business.categories.slice(0,3).map(category => (
-                            <div>{category}</div>
+                            <div key={category} className='biz-card-category'>{category}</div>
                         ))}
+                        <div style={{color: 'green'}}>{price}</div>
                     </div>
                     <div>
                         <div className='biz-description'>"{business.description}"</div>
