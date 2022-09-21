b2cf75545f9b856ad2b9d75c88d369d4f7c69e0a
Connor Lam <connorlam03@yahoo.com>
2022-09-21 11:11:16 -0700

    biz card done for now

--- react-app/src/components/Business/BusinessList/businessCard.js
+++ react-app/src/components/Business/BusinessList/businessCard.js
@@ -17,37 +17,36 @@ function BizCard({ business }) {
     const price = dollarsigns(business.priceRange);
     return (
         <div className='each-card'>
-            <NavLink className="flex-row navLink" to={`/businesses/${business.id}/about`}>
-                <div style={{height: "205px", minWidth:"205px"}}>
-                    <img src='https://wallpapercave.com/wp/wp2416473.jpg' style={{objectFit: "cover", height:"205px", width:"205px"}} alt=''/>
-                    {/* photo here */}
-                </div>
-                <div className='biz-card-right-side'>
-                    <div className='biz-card-name'> {business.name} </div>
-                    <div className='review-section'>
-                        <div class="stars-outer">
-                            <div class="stars-inner" style={{width: `${(business.avgReviews / 5) * 100}%`}}></div>
+                <NavLink className="flex-row navLink" to={`/businesses/${business.id}/about`}>
+                    <div style={{height: "205px", minWidth:"205px"}}>
+                        <img src='https://wallpapercave.com/wp/wp2416473.jpg' style={{objectFit: "cover", height:"205px", width:"205px"}} alt=''/>
+                        {/* photo here */}
+                    </div>
+                    <div className='biz-card-right-side'>
+                        <div className='biz-card-name'> {business.name} </div>
+                        <div className='review-section'>
+                            <div class="stars-outer">
+                                <div class="stars-inner" style={{width: `${(business.avgReviews / 5) * 100}%`}}></div>
+                            </div>
+                            <div className='num-reviews'>{business.numReviews ? business.numReviews : 'No reviews available'}</div>
                         </div>
-                        <div className='num-reviews'>{business.numReviews}</div>
+                        <div className='flex-row-align-center'>
+                            {business.categories.slice(0,3).map(category => (
+                                <>
+                                    {/* <div key={category} className='biz-card-category'>{category}</div> */}
+                                    <NavLink className='navLink biz-card-category' to={`/businesses/search?name=${category}&category=${category}`}>{category}</NavLink>
+                                </>
+                            ))}
+                            <div style={{color: 'green'}}>{price}</div>
+                        </div>
+                        <div>
+                            <div className='biz-description'>"{business.description}"</div>
+                        </div>
+                        <div>
+                            <div></div>
+                        </div>
                     </div>
-                    <div className='flex-row-align-center'>
-                        {business.categories.slice(0,3).map(category => (
-                            <>
-                                {/* <div key={category} className='biz-card-category'>{category}</div> */}
-                                <NavLink className='navLink biz-card-category' to={`/businesses/search?name=${category}&category=${category}`}>{category}</NavLink>
-                            </>
-                        ))}
-                        <div style={{color: 'green'}}>{price}</div>
-                    </div>
-                    <div>
-                        <div className='biz-description'>"{business.description}"</div>
-                    </div>
-                </div>
-
-
-
-
-            </NavLink>
+                </NavLink>
         </div>
         // <div>
         //     <NavLink to={`/businesses/${biz.id}`}>
