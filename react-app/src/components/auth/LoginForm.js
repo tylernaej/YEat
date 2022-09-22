8f63dae996291efa1f586f7fd044d7ef836f048d
Connor Lam <connorlam03@yahoo.com>
2022-09-21 23:56:52 -0700

    finished login and signup form css

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import { NavLink, Redirect } from 'react-router-dom';
 import { login } from '../../store/session';
 import './LoginForm.css'
 
@@ -42,38 +42,41 @@ const LoginForm = () => {
   }
 
   return (
-    <div id='full-login-page'>
-      <div className='login-to-yeat'>
+    <div id="full-login-page">
+      <div className="login-to-yeat">
         <h3>Log in to YEat</h3>
       </div>
-      <form onSubmit={onLogin}>
+      <form className="login-form" onSubmit={onLogin}>
         <div>
           {errors.map((error, ind) => (
-            <div key={ind}>{error}</div>
+            <div className='error' key={ind}><li>{error}</li></div>
           ))}
         </div>
-        <div>
-          <label htmlFor='email'>Email</label>
+        <div id="input-field">
+          {/* <label htmlFor="email">Email</label> */}
           <input
-            name='email'
-            type='text'
-            placeholder='Email'
+            name="email"
+            type="text"
+            placeholder="Email"
             value={email}
             onChange={updateEmail}
           />
         </div>
-        <div>
-          <label htmlFor='password'>Password</label>
+        <div id="input-field">
+          {/* <label htmlFor="password">Password</label> */}
           <input
-            name='password'
-            type='password'
-            placeholder='Password'
+            name="password"
+            type="password"
+            placeholder="Password"
             value={password}
             onChange={updatePassword}
           />
-          <div className='login-buttons'>
-            <button type='submit'>Login</button>
+          <div className="login-buttons">
+            <button type="submit">Login</button>
             <button onClick={handleDemo}>Login as Demo User</button>
+          </div>
+          <div className='new-to-yeat'>
+            <div><span className='new-to-yeat-words'>New to yeat?</span><NavLink className='new-to-yeat-link' to={'/signup'}>Sign Up!</NavLink></div>
           </div>
         </div>
       </form>
