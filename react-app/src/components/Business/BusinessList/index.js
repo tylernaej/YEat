a46c07cf0e2c72282b117bdb419168ed28b3acb2
tyler <tylernaej@gmail.com>
2022-09-24 22:37:46 -0400

    added spinner for transition between spash and all biz

--- react-app/src/components/Business/BusinessList/index.js
+++ react-app/src/components/Business/BusinessList/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from 'react-redux'
 import { useLocation } from "react-router-dom";
+import {ClimbingBoxLoader, PropagateLoader} from "react-spinners"
 import './index.css'
 
 
@@ -13,6 +14,7 @@ function BizList() {
     const dispatch = useDispatch()
     const businesses = useSelector(state => state.businesses)
     const businessList = Object.values(businesses)
+    const [spinLoaded, setSpinLoaded] = useState(false)
 
     const [isLoaded, setIsLoaded] = useState(false)
  
@@ -21,6 +23,24 @@ function BizList() {
         dispatch(getBizThunk())
         .then(() => setIsLoaded(true))
     }, [dispatch])
+
+    useEffect(() => {
+        setTimeout(() => {
+            setSpinLoaded(true)
+          }, "800")
+    }, [])
+
+    if(!spinLoaded){
+        return(
+            <div id='loader'>
+                <PropagateLoader 
+                    color="#ED161F" 
+                    size={20}
+                    speedMultiplier={1.7}
+                />
+            </div>
+        )
+    }
 
     return isLoaded && (
         <div className="bizlist-page">
