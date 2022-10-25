c69abd9ef1981f3233096096908d42fbd0d691a7
tyler <tylernaej@gmail.com>
2022-10-25 19:32:31 -0400

    more work on about page

--- react-app/src/components/About/About.js
+++ react-app/src/components/About/About.js
@@ -5,8 +5,8 @@ function About () {
 
 
     return(
-        <div>
-            <div>
+        <div id='about-exterior-container'>
+            <div id='about-interior-container'>
                 <h1>
                     Strong Coders:
                 </h1>
@@ -15,69 +15,78 @@ function About () {
                         <div id='developer-name'>
                             Connor Lam
                         </div>
+                        <div id='profile-picture-wrapper'>
+                            <img id='developer-profile-picture' src='https://avatars.githubusercontent.com/u/104233383?v=4' />
+                        </div>
                         <div>
-                            <div id='developer-profile-picture'>
-                                <img src='https://avatars.githubusercontent.com/u/104233383?v=4' />
-                            </div>
+                            bio
                         </div>
                         <div>
                             <div>Connect:</div>
                             <div id='developer-socials'>
-                                <div id='github-logo'><a><i className="fa-brands fa-github" /></a></div>
-                                <div id='linkedin-logo'><a><i className="fa-brands fa-linkedin" /></a></div>
-                                <div id='angelList-logo'><a><i className="fa-brands fa-angellist" /></a></div>
+                                <a href="https://github.com/ConnorLam" target="_blank" rel="noreferrer noopener" id='github-logo'><i className="fa-brands fa-github fa-4x" /></a>
+                                <a href='https://www.linkedin.com/in/connor-lam-a6545a23b/' target="_blank" rel="noreferrer noopener" id='linkedin-logo'><i className="fa-brands fa-linkedin fa-4x" /></a>
+                                <a href='https://angel.co/u/connor-lam' target="_blank" rel="noreferrer noopener" id='angelList-logo'><i className="fa-brands fa-angellist fa-4x" /></a>
                             </div>
                         </div>
                         <div>
                             <div>Other Projects:</div>
-                            <div></div>
-                            <div></div>
+                            <div id='projects-wrapper'>
+                                <div id='individual-project' ><a href='https://dropshotz.herokuapp.com/' target="_blank" rel="noreferrer noopener">DropShotz</a></div>
+                                <div id='individual-project' ><a href='https://airbnb-clone-connor-lam.herokuapp.com/' target="_blank" rel="noreferrer noopener">FakeBnb</a></div>
+                            </div>
                         </div>
                     </div>
                     <div id='individual-developer'>
                         <div id='developer-name'>
                             Edward Felipe III
                         </div>
+                        <div id='profile-picture-wrapper'>
+                            <img id='developer-profile-picture' src='https://media-exp1.licdn.com/dms/image/C5622AQE2ONk8C5o9hQ/feedshare-shrink_800/0/1666375144748?e=1669852800&v=beta&t=-Mh7n8h-ToxY0SMZx1azxcVL7aC7qdcs3IU56x4Pm9s' />
+                        </div>
                         <div>
-                            <div id='developer-profile-picture'>
-                                Picture
-                            </div>
+                            bio
                         </div>
                         <div>
                             <div>Connect:</div>
                             <div id='developer-socials'>
-                                <div id='github-logo'><a><i className="fa-brands fa-github" /></a></div>
-                                <div id='linkedin-logo'><a><i className="fa-brands fa-linkedin" /></a></div>
-                                <div id='angelList-logo'><a><i className="fa-brands fa-angellist" /></a></div>
+                                <a href='https://github.com/E-F-III' target="_blank" rel="noreferrer noopener" id='github-logo'><i className="fa-brands fa-github fa-4x" /></a>
+                                <a href='https://www.linkedin.com/in/efiii/' target="_blank" rel="noreferrer noopener" id='linkedin-logo'><i className="fa-brands fa-linkedin fa-4x" /></a>
+                                <a href='https://angel.co/u/e-f-iii' target="_blank" rel="noreferrer noopener" id='angelList-logo'><i className="fa-brands fa-angellist fa-4x" /></a>
                             </div>
                         </div>
                         <div>
                             <div>Other Projects:</div>
-                            <div></div>
-                            <div></div>
+                            <div id='projects-wrapper'>
+                                <div id='individual-project' ><a href='https://leitner-scape.herokuapp.com/' target="_blank" rel="noreferrer noopener">Leitner-scape</a></div>
+                                <div id='individual-project' ><a href='https://hike-up.herokuapp.com/' target="_blank" rel="noreferrer noopener">Hike-Up</a></div>
+                            </div>
                         </div>
                     </div>
                     <div id='individual-developer'>
                         <div id='developer-name'>
                             Tyler Jean
                         </div>
+                        <div id='profile-picture-wrapper'>
+                            <img id='developer-profile-picture' src='https://avatars.githubusercontent.com/u/104518737?s=400&u=62e1ffcb888bfa1a9ed7a13e82058c2718962c73&v=4' />
+                        </div>
                         <div>
-                            <div id='developer-profile-picture'>
-                                <img src='https://avatars.githubusercontent.com/u/104518737?s=400&u=62e1ffcb888bfa1a9ed7a13e82058c2718962c73&v=4' />
-                            </div>
+                            bio
                         </div>
                         <div>
                             <div>Connect:</div>
                             <div id='developer-socials'>
-                                <div id='github-logo'><a><i className="fa-brands fa-github" /></a></div>
-                                <div id='linkedin-logo'><a><i className="fa-brands fa-linkedin" /></a></div>
-                                <div id='angelList-logo'><a><i className="fa-brands fa-angellist" /></a></div>
+                                <a href="https://github.com/tylernaej" target="_blank" rel="noreferrer noopener" id='github-logo'><i className="fa-brands fa-github fa-4x" /></a>
+                                <a href='https://www.linkedin.com/in/tyler-jean-1a934ba8/' target="_blank" rel="noreferrer noopener" id='linkedin-logo'><i className="fa-brands fa-linkedin fa-4x" /></a>
+                                <a href='https://angel.co/u/tyler-jean' target="_blank" rel="noreferrer noopener" id='angelList-logo'><i className="fa-brands fa-angellist fa-4x" /></a>
                             </div>
                         </div>
                         <div>
                             <div>Other Projects:</div>
-                            <div></div>
-                            <div></div>
+                            <div id='projects-wrapper'>
+                                <div id='individual-project' ><a href='https://trello-tyler.herokuapp.com/home' target="_blank" rel="noreferrer noopener">Stratify</a></div>
+                                <div id='individual-project' ><a href='https://airbnb-tyler.herokuapp.com/' target="_blank" rel="noreferrer noopener">Airbnb</a></div>
+                            </div>
                         </div>
                     </div>
                 </div>
